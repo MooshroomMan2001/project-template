9ccce46e42e3a2022b7c7d6fead404c621cbd59d
MooshroomMan2001 <ewb@unassigned-hostname>
2014-02-11 17:53:31 -0500

    Added variables

--- project.js
+++ project.js
@@ -1,40 +1,37 @@
 var sketchProc = function(processing) {
     var img = processing.loadImage("Background.png");
 	
-    // put variables here, just like kahn academy
-    var exampleVar = 10;
+    // put variables anywhere
+    var textY = 300
+    var textX = 300
 
     // put functions here, just like kahn academy
-    var exampleFunction = function(){
 
-    };
 
     // this code is executed once when the program is started
     processing.setup = function() {
 
 	// set up the size of the canvas (you probably don't want to change this!)
-	processing.size(800, 600);
+	processing.size(625, 450);
 
 	// some drawing commands; feel free to change these
 	processing.fill(255, 255, 0);
 
 	// draw some text
-
-	// call a function (defined up above)
-	exampleFunction();
     };
 
     // override draw function, by default it will be called 60 times per second
     processing.draw = function() {
 	processing.image(img, 0, 0);
 	processing.rect(mouseX, mouseY, 30, 30);
-	if(mouseX >= 750 && mouseX <= 0){
+	if(mouseX >= 625 && mouseX <= 0){
 	    processing.textSize(90, 80);
-	    processing.text("You died!", 300, 300);
+	    processing.fill(255, 0, 0);
+	    processing.text("You died!", textX, textY );
 	}
-	if(mouseY >= 500){
+	if(mouseY >= 450){
 	    processing.textSize(90, 30);
-	    processing.text("You died!", 300, 300);
+	    processing.text("You died!", textX, textY);
 	}
 	
     };
